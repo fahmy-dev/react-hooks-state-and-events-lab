dce9bd0e4202e2555cf81456b96b11fc523b4df0
fahmy-dev <faahmy@proton.me>
2025-01-20 12:15:11 +0300

    initial commit

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setInCart] = useState(false)
+  function toggleCart() {
+    setInCart((previsInCart) => !previsInCart);
+  }
+
+  const liClass = isInCart ? "in-cart" : "";
+  const buttonText = isInCart ? "Remove From Cart" : "Add To Cart"
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={toggleCart}>{buttonText}</button>
     </li>
   );
 }
